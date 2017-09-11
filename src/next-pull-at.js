d95c3102e244db195e71a85fae956115e7cb3ca7
afeiship <1290657123@qq.com>
2017-09-11 10:14:43 +0800

    issue: optmize code

--- src/next-pull-at.js
+++ src/next-pull-at.js
@@ -7,19 +7,19 @@
 
   nx.pullAt = function (inArray) {
 
-    var i, j, index;
-    var length = inArray.length;
     var result = [];
+    var i, index;
     var args_ = ARRAY_PROTO.slice.call(arguments, 1);
     var len = args_.length;
+    var length = inArray.length;
     var value, element;
 
     for (i = 0; i < len; i++) {
       value = args_[i];
-      for (j = 0; j < length; j++) {
-        element = inArray[j];
-        if (value === j) {
-          inArray.splice(j, 1, PLACRHOLDER);
+      for (index = 0; index < length; index++) {
+        element = inArray[index];
+        if (value === index) {
+          inArray.splice(index, 1, PLACRHOLDER);
           result.push(element);
           break;
         }
